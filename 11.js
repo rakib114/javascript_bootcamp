3de23cf37d98d5a03838b1c7c6aa6c1cf6136e9a
rakib114 <mahmud17209@gmail.com>
2022-03-07 15:57:55 +0600

    boolean & comparison operator

---
+++ 11.js
@@ -0,0 +1,33 @@
+// Boolean & comparison operator
+
+// booleans
+// true & false
+
+let num1 = 8,
+  num2 = 8;
+
+console.log(num1 >= num2);
+
+// == vs ===
+
+let num3 = "9",
+  num4 = 9;
+
+// == check only values not data type
+
+console.log(num3 == num4);
+
+// === check values and also data type;
+
+console.log(num3 === num4);
+
+// != vs !==
+
+let num5 = 3,
+  num6 = "3";
+
+// != check only values not data types
+console.log(num5 != num6);
+
+// !== check values and alos data types
+console.log(num5 !== num6);
