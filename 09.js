4f770207b9be74dd97e96a1e41fef7f85f511dcf
rakib114 <mahmud17209@gmail.com>
2022-03-07 13:44:33 +0600

    Template String

---
+++ 09.js
@@ -0,0 +1,15 @@
+// template string
+
+let nam = "Mohit";
+let age = 23;
+
+// My name is Mohit and I'm 23 years old
+
+let aboutMe = "My name is" + " " + nam + "I'm " + age + " years old";
+
+// console.log(aboutMe);
+
+// Same thing using temlate string
+
+aboutMe = `My name is ${nam} and I'm ${age} years old`;
+console.log(aboutMe);
