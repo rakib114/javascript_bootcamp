f3c75b613c271fca203ee52927b0055b6ed3257d
rakib114 <mahmud17209@gmail.com>
2022-03-07 16:53:18 +0600

    nasted if else

---
+++ 15.js
@@ -0,0 +1,14 @@
+// nasted if else
+
+let winingNumber = 21,
+  userGase = 11;
+
+if (userGase === winingNumber) {
+  console.log("Your gause is right");
+} else {
+  if (userGase > winingNumber) {
+    console.log("Too High...");
+  } else {
+    console.log("Too low..");
+  }
+}
