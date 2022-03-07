1470ad1c73b8407a3e3bfb6ec75b54dab06ea8b1
rakib114 <mahmud17209@gmail.com>
2022-03-07 14:01:56 +0600

    undefien & null & BigInt

---
+++ 10.js
@@ -0,0 +1,21 @@
+// undefien
+// null
+
+// underied
+let firstName;
+console.log(typeof firstName);
+
+// null
+let hudaiVariable = null;
+console.log(hudaiVariable);
+console.log(typeof hudaiVariable);
+// output e data type object dekhano ta ekta bugs;
+
+// BigInt
+// BigInt er sahte normal number oparet kora jay nah
+// Big Int er sahte BigINt eri + / - .. korte hoy
+let myNum = BigInt(1234);
+console.log(myNum);
+
+// to check how much normal number cant store
+console.log(Number.MAX_SAFE_INTEGER);
