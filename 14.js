7f59df0b98f409d2d164d7c47e3d7ea10ef052c9
rakib114 <mahmud17209@gmail.com>
2022-03-07 16:33:12 +0600

    ternary or conditional operator

---
+++ 14.js
@@ -0,0 +1,18 @@
+// ternary / conditional operator
+
+// let age = 9,
+//   drink;
+
+// if (age >= 6) {
+//   drink = "Coffee";
+// } else {
+//   drink = "Milk";
+// }
+// console.log(`Your Drink is ${drink}`);
+
+// Uporer condition using ternary operator
+
+let age = 2,
+  drink = age >= 6 ? "Coffee" : "Milk";
+
+console.log(`Your Drink is ${drink}`);
