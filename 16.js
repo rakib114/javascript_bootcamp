5211a935c9adfb4f041e2c7c4391afcaa7a6d6c4
rakib114 <mahmud17209@gmail.com>
2022-03-11 21:25:50 +0600

    Array

---
+++ 16.js
@@ -0,0 +1,19 @@
+// Intro to arrays
+// reference data type
+//
+
+// ordered collection of iteam
+let fruties = ["Mango", "Apple", "Orange", "Banana"];
+console.log(typeof fruties);
+console.log(fruties);
+
+fruties[3] = "Strobery";
+console.log(fruties);
+
+// Check Array
+console.log(Array.isArray(fruties));
+
+// We can store all data type in a Array
+
+let mixed = [1, 2, 3.4, null, undefined, "String"];
+console.log(mixed);
