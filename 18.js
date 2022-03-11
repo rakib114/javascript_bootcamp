cbe5fd9eb4f2a26f4b5b822f0dc0a98531e03d01
rakib114 <mahmud17209@gmail.com>
2022-03-11 22:08:52 +0600

    Premitive VS reference data type

---
+++ 18.js
@@ -0,0 +1,22 @@
+// Premitive VS reference data type
+
+// Premitive Data type
+let num1 = 5;
+let num2 = num1;
+console.log(num1);
+console.log(num2);
+// After incrementing num1 value
+num1++;
+console.log(num1);
+console.log(num2);
+
+// Reference data type
+
+let array1 = ["Rohit", "Mohit"];
+let array2 = array1;
+console.log(array1);
+console.log(array2);
+// After pushin a iteam in array1
+array1.push("Bhuvan");
+console.log(array1);
+console.log(array2);
