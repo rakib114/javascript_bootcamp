2f03e2a7e76b87ba37387e6d180ce62c2dadf33c
rakib114 <mahmud17209@gmail.com>
2022-03-14 20:09:39 +0600

    for of loop in array

---
+++ 21.js
@@ -0,0 +1,9 @@
+// for of loop in array
+
+let players = ["Sakib", "Tamim", "Mashrafi", "Mushfiq"];
+let cnfList = [];
+for (const player of players) {
+  //   console.log(player.toUpperCase());
+  cnfList.push(player.toUpperCase());
+}
+console.log(cnfList);
