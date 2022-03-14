64e94f3dc127359c8f43f3459a1a5f45bd9d3678
rakib114 <mahmud17209@gmail.com>
2022-03-14 19:56:52 +0600

    for loop in array

---
+++ 20.js
@@ -0,0 +1,9 @@
+// for loop in array
+
+let fruits = ["Mango", "Apple", "Banana", "Pineapple"];
+let fruits2 = [];
+for (let i = 0; i < fruits.length; i++) {
+  //   console.log(fruits[i].toUpperCase());
+  fruits2.push(fruits[i].toUpperCase());
+}
+console.log(fruits2);
