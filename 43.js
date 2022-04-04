d8dd4cb9af0243a17cceea6f14fb3c3e0944dc6b
rakib114 <mahmud17209@gmail.com>
2022-04-04 16:03:58 +0600

    fill Method

---
+++ 43.js
@@ -0,0 +1,9 @@
+// fill Method
+
+const myArray = new Array(9).fill(3);
+console.log(myArray);
+
+// value, start, end
+const myArray2 = [1, 38, 9, 8, 3, 2, 4];
+const num = myArray2.slice(0).fill(0, 2, 5);
+console.log(num);
