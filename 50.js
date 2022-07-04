d874f83734a5e3b655d821a41d634abf9daaca5e
rakib114 <mahmud17209@gmail.com>
2022-07-04 22:28:14 +0600

    When we use new keyword we should start our function name with capital letter

--- 50.js
+++ 50.js
@@ -3,29 +3,29 @@
 // 2.) return full object {}
 // link __proto__ with prototype
 
-function createUser(name, email, age, address) {
+function CreateUser(name, email, age, address) {
   this.name = name;
   this.email = email;
   this.age = age;
   this.address = address;
-  this.about = createUser.prototype.about;
-  this.is18 = createUser.prototype.is18;
+  this.about = CreateUser.prototype.about;
+  this.is18 = CreateUser.prototype.is18;
 }
 // console.log(createUser.prototype);
 
-createUser.prototype.about = function () {
+CreateUser.prototype.about = function () {
   return `${this.name} is ${this.age} old Fvrt Band ${this.fvrtBnd()} `;
 };
-createUser.prototype.is18 = function () {
+CreateUser.prototype.is18 = function () {
   return this.age >= 18;
 };
-createUser.prototype.fvrtBnd = function () {
+CreateUser.prototype.fvrtBnd = function () {
   return "BTS";
 };
-const user1 = new createUser("Mahim", "mahima@gmail.com", 20, "Tangail");
+const user1 = new CreateUser("Mahim", "mahima@gmail.com", 20, "Tangail");
 // console.log(user1);
 console.log(user1.about());
 console.log(user1.fvrtBnd());
 
-const user2 = new createUser("Rakib", "imark@gmail.com", 21, "Savar");
+const user2 = new CreateUser("Rakib", "imark@gmail.com", 21, "Savar");
 console.log(user2);
